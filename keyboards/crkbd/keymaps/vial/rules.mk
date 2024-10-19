a140cf1e5fc8d9abd5e772dc597a51c857d2ead5
U-XX2\Thibkpad <williamgunawaneka@gmail.com>
2024-10-19 16:30:12 +0800

    tmp

--- keyboards/crkbd/keymaps/vial/rules.mk
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -2,7 +2,7 @@ VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
 LTO_ENABLE          = yes
 
-RGBLIGHT_ENABLE     = yes
+RGBLIGHT_ENABLE     = no
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 MOUSEKEY_ENABLE     = no
 OLED_ENABLE         = no
@@ -11,3 +11,4 @@ EXTRAKEY_ENABLE     = no
 COMBO_ENABLE        = no
 
 QMK_SETTINGS        = no
+SERIAL_DRIVER       = vendor
